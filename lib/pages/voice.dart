a2a6c0c57c09e6ebf7b8147d73018505a656931c
Ravi <ravi5gangster@gmail.com>
2026-06-13 19:37:09 +0530

    UInput AppOpen

--- lib/pages/voice.dart
+++ lib/pages/voice.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:installed_apps/installed_apps.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:voicein/services/appLauncher_service.dart';
 
 class Voice extends StatefulWidget {
   const Voice({super.key});
@@ -9,27 +11,92 @@ class Voice extends StatefulWidget {
 }
 
 class _VoiceState extends State<Voice> {
+  final SpeechToText stx = SpeechToText();
+  String inputedText = "input text here show";
+  //start the system speech engine
+  @override
+  void initState() {
+    super.initState();
+    speechinit();
+  }
+
+  //check for microphone and speech text engine
+  Future<void> speechinit() async {
+    await stx.initialize();
+  }
+
+  Future<void> speechlisten() async {
+    await stx.listen(
+      onResult: (result) {
+        stx.listen(
+          onResult: (result) {
+            setState(() {
+              inputedText = result.recognizedWords;
+              //this will open the gmail
+              String command = result.recognizedWords.toLowerCase();
+              if (command.contains("gmail")) {
+                ApplauncherService.openGmail();
+              } else if (command.contains("whatsapp")) {
+                ApplauncherService.openWhatsapp();
+              } else {
+                inputedText = "no input";
+              }
+            });
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Voice Assistant"),
+        title: const Text(
+          "Voice Assistant",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
-        backgroundColor: Colors.pink,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          InkWell(
-            onTap: () {
-              InstalledApps.startApp('com.whatsapp');
-            },
-            child: CircleAvatar(
-              backgroundColor: Colors.pink,
-              child: Icon(Icons.mail_rounded, color: Colors.white),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //for tapping the container
+            GestureDetector(
+              onTap: () {
+                speechlisten();
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(Icons.mic, color: Colors.white, size: 60),
+              ),
             ),
-          ),
-        ],
+
+            SizedBox(height: 30),
+            //hint text for the user
+            Text(
+              "Tap To Speak",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 30),
+            // show the user input text
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                inputedText,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 22),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
