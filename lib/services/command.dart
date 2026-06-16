199015059bd563ea568179e4fd61305798671afa
Ravikant Vishwakarma <119119059+Ravi2006kant@users.noreply.github.com>
2026-06-16 19:33:24 +0530

    Final new

--- lib/services/command.dart
+++ lib/services/command.dart
@@ -1,29 +1,39 @@
-import 'package:voicein/services/appLauncher_service.dart';
 import 'package:voicein/services/email_service.dart';
+import 'package:voicein/services/appLauncher_service.dart';
 
 class Command {
   static void process(String command) {
     command = command.toLowerCase();
-    final emailRegex = RegExp(
-      r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}',
-    );
 
-    final match = emailRegex.firstMatch(command); // Send email to xyz@gmail.com
     if (command.contains("send email")) {
+      final emailRegex = RegExp(
+        r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}',
+      );
+
+      final match = emailRegex.firstMatch(command);
+
       if (match != null) {
         String email = match.group(0)!;
+
         String subject = "";
         String body = "";
 
         if (command.contains("subject")) {
-          subject = command.split("subject")[1].split("body")[0].trim();
+          subject = command
+              .split("subject")[1]
+              .split("body")[0]
+              .trim();
         }
 
         if (command.contains("body")) {
           body = command.split("body")[1].trim();
         }
 
-        EmailService.composeEmail(email, subject: "", body: "");
+        EmailService.composeEmail(
+          email,
+          subject: subject,
+          body: body,
+        );
       }
 
       return;
@@ -31,12 +41,10 @@ class Command {
 
     if (command.contains("open gmail")) {
       ApplauncherService.openGmail();
-      return;
     }
 
     if (command.contains("open whatsapp")) {
       ApplauncherService.openWhatsapp();
-      return;
     }
   }
 }
