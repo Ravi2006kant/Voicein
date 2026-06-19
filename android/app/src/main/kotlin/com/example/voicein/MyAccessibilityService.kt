148a6306b7707fa3a1fe767f269eb220e2b88254
Ravi <ravi5gangster@gmail.com>
2026-06-19 13:11:12 +0530

    take

--- android/app/src/main/kotlin/com/example/voicein/MyAccessibilityService.kt
+++ android/app/src/main/kotlin/com/example/voicein/MyAccessibilityService.kt
@@ -1,13 +1,26 @@
 package com.example.voicein
 
 import android.accessibilityservice.AccessibilityService
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 
 class MyAccessibilityService : AccessibilityService() {
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        Log.d("ACCESSIBILITY", "SERVICE CONNECTED")
     }
 
-    override fun onInterrupt() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+        val packageName = event?.packageName?.toString()
+
+        Log.d("ACCESSIBILITY", "APP: $packageName")
+
+        if (packageName == "com.google.android.gm") {
+            Log.d("ACCESSIBILITY", "GMAIL DETECTED")
+        }
     }
+
+    override fun onInterrupt() {}
 }
